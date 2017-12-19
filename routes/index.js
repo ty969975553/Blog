a1bbd421d0796b2621a1cb801a1b4c4da5413b3c
一潭清泓 <969975553@qq.com>
2017-12-19 19:00:51 +0800

    首次提交
    1219

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var user = require('../services/User');
+var moment = require('moment');
+var tags = require('../services/Tags');
+var article = require('../services/Article');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '首页' });
+});
+
+router.get('/index', function(req, res, next) {
+    res.render('index', { title: '首页' });
+});
+
+
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: '关于' });
+});
+
+/* GET home page. */
+router.get('/activity', function(req, res, next) {
+    res.render('activity', { title: '动态' });
+});
+
+router.get('/experience', function(req, res, next) {
+    res.render('experience', { title: '学习经历' });
+});
+
+router.get('/contact', function(req, res, next) {
+    res.render('contact', { title: '联系' });
+});
+router.get('/work', function(req, res, next) {
+    res.render('work', { title: '工作' });
+});
+
+module.exports = router;
